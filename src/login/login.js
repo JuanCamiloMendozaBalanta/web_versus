213d0716440e8b24046cc45f17f02ccc3345a9e8
junior25 <jaimea@clientmaker.com>
2020-01-14 23:58:16 -0500

    work on login

--- src/login/login.js
+++ src/login/login.js
@@ -4,10 +4,15 @@ import React from "react";
 //STYLE
 import "./login.scss";
 
+//IMAGES
+import logo2 from '../assets/logo2.jpg'
+
 class Login extends React.PureComponent {
     render() {
         return <div className="Login-box">
-            <div className="Left"></div>
+            <div className="Left">
+            <img className="Left-logo" src={logo2} alt="logo"/>
+            </div>
             <div className="Right"></div>
         </div>
     }
