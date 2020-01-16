780e3c61089f00adcdd1d4098eee4cf6c3b63b73
Juan Camilo Mendoza Balanta <juancamilomendozabalanta.jcmb@gmail.com>
2020-01-15 21:58:52 -0500

    save on login

--- src/login/login.js
+++ src/login/login.js
@@ -4,19 +4,38 @@ import React from "react";
 //STYLE
 import "./login.scss";
 
-//IMAGES
-import logo2 from '../assets/logo2.jpg'
-
 class Login extends React.PureComponent {
-    render() {
-        return <div className="Login-box">
-            <div className="Left">
-            <img className="Left-logo" src={logo2} alt="logo"/>
+  render() {
+    return (
+      <div className="Login-box">
+        <div className="Wrap">
+          <div className="Form-box">
+            <div className="Form-title">
+              <div className="Form-title-welcome">Welcome to</div>
+              <div className="Form-title-versus">versus</div>
             </div>
-            <div className="Right"></div>
+            <form className="Form-info">
+              <label className="Form-info-label">Email</label>
+              <input className="Form-info-input" id="email" type="email" />
+              <label className="Form-info-label">Password</label>
+              <input
+                className="Form-info-input"
+                id="password"
+                type="password"
+              />
+              <button className="Form-info-button" type="submit">
+                LOGIN
+              </button>
+            </form>
+            <div className="Form-singup">
+              <div className="Form-sigup-text">Dont have an account ? </div>
+              <a className="Form-sigup-link">Sign up</a>
+            </div>
+          </div>
         </div>
-    }
-
+      </div>
+    );
+  }
 }
 
-export default Login
+export default Login;
