57b5be62ef47f32fc321478c46620ead5936fb64
junior25 <jaimea@clientmaker.com>
2020-01-26 16:08:53 -0500

    work on signup

--- src/home/home.js
+++ src/home/home.js
@@ -6,10 +6,13 @@ import {Switch, Route} from "react-router-dom";
 
 //COMPONENTS
 import Login from '../login/login'
+import Signup from '../signup/signup'
+
 const Home = () => {
   return <div className="Home-box">
     <Switch>
       <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
     </Switch>
   </div>;
 
--- src/signup/signup.js
+++ src/signup/signup.js
@@ -6,15 +6,12 @@ import "./signup.scss";
 
 
 const Signup = () => {
-  const responseGoogle = response => {
-    console.log("===>", response);
-  };
   return (
     <div className="Signup-box">
       <div className="Wrap">
         <div className="Signup-header">
-          <div className="Signup-header-gettingStarted">Geting Started</div>
-          <div className="Form-title-welcome">Already have an account</div>
+          <div className="Signup-header-gettingStarted">Getting Started</div>
+          <div className="Signup-header-aha">Already have an account</div>
         </div>
         <div className="Form-box">
         </div>
